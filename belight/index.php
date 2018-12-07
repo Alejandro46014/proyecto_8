f33f13f6ee3bd54b73f50e5850b22eb41a47ec01
belight <alejandroalcaraz@fp.uoc.edu>
2018-12-08 00:50:24 +0100

    Estructura principal plantillas,css y js 2

--- belight/index.php
+++ belight/index.php
@@ -1,5 +1,5 @@
 <?php require_once 'vistas/plantillas/header.php';  ?>
 
-<?php require_once 'vistas/plantillas/navegacion_usuario.php';  ?>
+<?php require_once 'vistas/plantillas/navegacion_usuarios.php';  ?>
 
 <?php require_once 'vistas/plantillas/footer.php';  ?>
