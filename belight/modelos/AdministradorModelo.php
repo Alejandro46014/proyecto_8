aa150ac5aff40f35045cbf9d23d544b1665f47cb
belight <alejandroalcaraz@fp.uoc.edu>
2018-12-14 01:11:56 +0100

    AdministradorControlador y AdministradorModelo

--- belight/modelos/AdministradorModelo.php
+++ belight/modelos/AdministradorModelo.php
@@ -4,88 +4,12 @@ class AdministradorModelo extends UsuariosModelo{
 	
 	public function __construct(){
 		parent::__construct();
-		$this->tipo_usuario="Administrador";
-	}
-	
-	/*---------------------------LISTAR USUARIOS ACTIVOS O NO--------------------------*/
-	
-	public function listarUsuarios($activo){
-		require_once("ConectarModelo.php");
-		try{
-			$conexion=ConectarModelo::conexion();
-			$activo=$_POST['activo'];
-			$sql="SELECT * FROM usuarios WHERE acivo=:activo";
-			
-			$consulta=$conexion->prepare($sql);
-			
-			$consulta->binParam(':activo',$activo,PDO::PARAM_STR);
-			$consulta->execute();
-			$resultado=$consulta->fetchAll();
-			
-			$consulta->closeCursor();
-			
-		}catch(PDOException $e){
-			
-			die("No se pudo conectar con la BBDD ".$e->getMessage());
-			echo("Linea de error ".$e->getLine());
-		}
-		
-		$conexion=null;
-		
-		return($resultado);
+		$this->tipo_usuario->SetIdTipoUsuario(1);
 	}
 	
 	
-	/*---------------------------BLOQUEAR USUARIO ADMINISTRADOR---------------------*/
-	
-	public function bloquearUsuario($usuario){
-		
-		require_once("ConectarModelo.php");
-		try{
-			
-			$id= $usuario->id;
-                        $activo= $usuario->activo_usuario;
-                        
-			$conexion=ConectarModelo::conexion();
-			$sql="UPDATE usuarios SET activo_usuario=:activo WHERE id_usuario=:id";
-			
-			$consulta=$conexion->prepare($sql);
-			
-                        $consulta->bindParam(':id',$id,PDO::PARAM_INT);
-			$consulta->bindParam(':activo',$activo,PDO::PARAM_STR);
-			
-			$resultado=$consulta->execute();
-			
-			$consulta->closeCursor();
-			
-			if($resultado){
-				
-				echo '<script type="text/javascript">
-				alert("El usuario se desactivó correctamente");
-				</script>';
-			
-				
-			}else{
-				
-				echo '<script type="text/javascript">
-				alert("El usuario no se pudo desactivar");
-				</script>';
-			
-				
-			}
-			
-		}catch(PDOException $e){
-			
-			die("No se pudo conectar con la BBDD ".$e->getMessage());
-			echo("Linea de error ".$e->getLine());
-		}
-		
-		$conexion=null;
-		
-		return($resultado);
-	}
         
-        public function buscarUsuarios($id,$tipo_usuario,$activo){
+        public function buscarUsuarios($id,$pais){
             
             require_once 'modelos/ConectarModelo.php';
             
@@ -94,12 +18,12 @@ class AdministradorModelo extends UsuariosModelo{
                 $conexion= ConectarModelo::conexion();
                 $listausuarios=[];
                  $sql="SELECT * FROM usuarios  INNER JOIN tipo_usuarios ON tipo_usuarios_id_tipo_usuario=id_tipo_usuario WHERE"
-                        . " id_usuario LIKE :id OR tipo_usuarios_id_tipo_usuario LIKE :tipo OR activo_usuario LIKE :activo";
+                        . " id_usuario LIKE :id OR pais_usuario LIKE :pais ";
                 $consulta=$conexion->prepare($sql);
                 
                 $consulta->bindParam(':id',$id,PDO::PARAM_INT);
-                $consulta->bindParam(':tipo',$tipo_usuario,PDO::PARAM_STR);
-                $consulta->bindParam(':activo',$activo,PDO::PARAM_STR);
+                $consulta->bindParam(':pais',$pais,PDO::PARAM_STR);
+               
                 $consulta->execute();
                 $resultado=$consulta->fetchAll();
                 
@@ -113,16 +37,28 @@ class AdministradorModelo extends UsuariosModelo{
                     foreach ($resultado as $fila){
                         
                         $usuario=new UsuariosModelo();
-                        
-                        $usuario->id=$fila['id_usuario'];
-                        $usuario->nombre_usuario=$fila['nombre_usuario'];
-                        $usuario->apellidos_usuario=$fila['apellidos_usuario'];
-                        $usuario->fecha_nacimiento_usuario=$fila['fecha_nacimiento_usuario'];
-                        $usuario->tipo_usuario=new TipoUsuarios($fila['tipo_usuarios_id_tipo_usuario']);
-                        $usuario->fecha_alta_usuario=$fila['fecha_alta_usuario'];
-                        $usuario->pais_usuario=$fila['pais_usuario'];
-                        $usuario->email_usuario=$fila['email_usuario'];
-                        $usuario->activo_usuario=$fila['activo_usuario'];
+					
+					$usuario->id_usuario=$fila['id_usuario'];
+                                        $usuario->dni_usuario=$fila['dni_usuario'];
+					$usuario->nombre_usuario=$fila['nombre_usuario'];
+					$usuario->apellidos_usuario=$fila['apellidos_usuario'];
+					$usuario->email_usuario=$fila['email_usuario'];
+					$usuario->password_usuario=$fila['password_usuario'];
+					$usuario->tipo_usuario=new TipoUsuarios();
+					
+                                        $direccion=new DireccionUsuariosModelo();
+                                        
+                                        $direccion->setIdDireccion($fila['id_direccion']);
+                                        $direccion->setPaisUsuario($fila['pais_usuario']);
+                                        $direccion->setCiudadUsuario($fila['ciudad_usuario']);
+                                        $direccion->setPoblacionUsuario($fila['poblacion_usuario']);
+                                        $direccion->setCalleUsuario($fila['calle_usuario']);
+                                        $direccion->setNCalleUsuario($fila['n_calle_usuario']);
+                                        $direccion->setEscaleraUsuario($fila['escalera_usuario']);
+                                        $direccion->setCpUsuario($fila['cp_usuario']);
+                                        $direccion->setTelfUsuario($fila['telefono_usuario']);
+                                        
+                                        $usuario->setDireccionUsuario($direccion);
                         
                         $listausuarios[]=$usuario;
                     } 
