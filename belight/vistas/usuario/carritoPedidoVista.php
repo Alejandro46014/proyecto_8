317a691357eb8e07ff0f1a83c0d5391f13e1fd46
Alejandro <aalcarazma@fp.uoc.edu>
2018-12-17 19:40:41 +0100

    Estilos carrito y CarritoPedidoVista

--- belight/vistas/usuario/carritoPedidoVista.php
+++ belight/vistas/usuario/carritoPedidoVista.php
@@ -10,21 +10,36 @@
           
             
             <div class="datos_producto">
-         
+                <div class="imagen">
                 <img src="<?php echo $producto->getImagenProducto();  ?>" alt="<?php echo $producto->getNombreProducto();  ?>"/>
+                </div>
                 <div class="producto">
                     
                     <h4><?php echo $producto->getNombreProducto(); ?></h4>
                 </div>
             </div>
             
-            <div class="datos_usuario">
+            <div class="datos_usuario clearfix">
                 <div class="usuario">
-                    
+                    <h4>Datos personales</h4>
+                    <ul>
+                        <li>Nombre: <?php echo $usuario->getNombreUsuario(). " ".$usuario->getApellidosUsuario(); ?></li>
+                        <li>Nº teléfono: <?php echo $usuario->getDireccionUsuario()->getTelfUsuario(); ?></li>
+                    </ul>
                     
                 </div>  
                 <div class="usuario">
                     
+                    <h4>Dirección</h4>
+                    <ul>
+                        <li>Calle: <?php echo $usuario->getDireccionUsuario()->getCalleUsuario(); ?></li>
+                        <li>Nº calle: <?php echo $usuario->getDireccionUsuario()->getNCalleUsuario(); ?></li>
+                        <li>Escalera: <?php echo $usuario->getDireccionUsuario()->getEscaleraUsuario(); ?></li>
+                        <li>Código postal: <?php echo $usuario->getDireccionUsuario()->getCpUsuario(); ?></li>
+                        <li>Población: <?php echo $usuario->getDireccionUsuario()->getPoblacionUsuario(); ?></li>
+                        <li>Ciudad: <?php echo $usuario->getDireccionUsuario()->getCiudadUsuario(); ?></li>
+                        <li>País: <?php echo $usuario->getDireccionUsuario()->getPaisUsuario(); ?></li>
+                    </ul>
                     
                 </div>  
                 
@@ -39,7 +54,12 @@
                     </ul>
                 </div>
             </div>
-            
+            <div class="col_formulario">
+                <input type="submit" name="confirmar_carrito" value="Confirmar" class="buttom_green"/>
+            </div>
+             <div class="col_formulario">
+                <input type="submit" name="vaciar_carrito" value="Cancelar" class="buttom_red"/>
+            </div>
         
         </form>
     </div>
